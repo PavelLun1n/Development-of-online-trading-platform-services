f9def102dce13e1871210bac378cc354842dd223
PavelLun1n <106957514+PavelLun1n@users.noreply.github.com>
2025-05-07 01:51:22 +0300

    Delete src/main directory

--- src/main/java/com/example/repository/SellerRatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.SellerRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SellerRatingRepository extends JpaRepository<SellerRating, Long> {
-}
